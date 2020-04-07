ec7fbae6326703b69ccfe6f4b0812f9e136145d9
Ryan Gautier <ryangautier2@gmail.com>
2020-04-07 00:20:52 -0500

    add continue and nav btns, change tab when scroll

--- logic.js
+++ logic.js
@@ -3,13 +3,23 @@ $(document).ready(function () {
     var navPosition = $("#navbar").offset().top;
 
     $(window).scroll(function () {
+        // if the window is at the bottom of the cover page
         if ($(window).scrollTop() >= navPosition) {
+            // make navbar sticky
             $("#navbar").addClass("sticky");
             $("#cover-screen").addClass("extra-margin");
+            
+            // change active tab on nav bar
+            $("#home-link").removeClass("current-page");
+            $("#resume-link").addClass("current-page");
         }
         else {
             $("#navbar").removeClass("sticky");
             $("#cover-screen").removeClass("extra-margin");
+
+            // change active tab on nav bar
+            $("#home-link").addClass("current-page");
+            $("#resume-link").removeClass("current-page");
         }
 
     });
